01e43c8b2754941ec66aedf861a1daa22447b687
linkai <songlinkai03@gmail.com>
2025-05-29 03:05:51 +0800

    adding basic configs for nvim

---
+++ config/nvim/lua/options.lua
@@ -0,0 +1,39 @@
+vim.g.encoding = "UTF-8"
+vim.opt.fileencoding = "UTF-8"
+
+vim.opt.clipboard = 'unnamedplus'
+vim.opt.completeopt = { 'menu', 'menuone', 'noselect' }
+vim.opt.mouse = 'a'
+
+vim.opt.cursorline = true
+vim.opt.scrolloff = 8
+vim.sidescrolloff = 8
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.showmode = false
+
+vim.opt.signcolumn = "yes"
+
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 0
+
+vim.opt.autoindent = true
+vim.bo.autoindent = true
+vim.opt.smartindent = true
+
+vim.opt.backup = false
+vim.opt.writebackup = false
+vim.opt.swapfile = false
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.incsearch = true
+
+vim.opt.pumheight = 10
+vim.g.completopt = "menu,menuone,noselect,noinsert"
+
+vim.opt.autoread = true
